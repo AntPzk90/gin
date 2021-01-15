ad0129008989b513d22aa82ab7f18aedf479ff21
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2021-01-15 11:02:39 +0200

    add mobile version

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ let reviewsSwiper = new Swiper('.main-slider__container', {
     clickable: true,
   },
 });
+
+$('.header__burger').click(function() {
+  $(this).toggleClass('header__burger--active');
+  $('.bottom-nav__list').slideToggle(150).css('display', 'flex');
+  if(window.matchMedia("screen and (max-width: 768px)").matches) {
+    $('.main-nav:not(.main-nav--footer)').slideToggle(150).css('display', 'flex');
+  }
+});
+
+
